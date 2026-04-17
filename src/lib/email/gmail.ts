518a0dd816a97b394df33e127838d89bedc66b79
Scott White <scottaw33@gmail.com>
2026-04-17 22:16:52 +1000

    feat(compose): email-account signature + font fields

--- src/lib/email/gmail.ts
+++ src/lib/email/gmail.ts
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 const SCOPES = [
   "https://www.googleapis.com/auth/gmail.send",
   "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/gmail.settings.basic",
   "https://www.googleapis.com/auth/userinfo.email",
 ];
 
